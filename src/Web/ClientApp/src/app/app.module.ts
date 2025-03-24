ee95ee0aed0d2cec4f970137d8fbf0230585dc3b
v.syrov <v.syrov@pix.ru>
2025-03-24 16:09:02 +0300

    Удалены шаблонные файлы на фронтэнде

--- src/Web/ClientApp/src/app/app.module.ts
+++ src/Web/ClientApp/src/app/app.module.ts
@@ -13,33 +13,22 @@ import {
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { TodoComponent } from './todo/todo.component';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    TodoComponent,
-  ],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'todo', component: TodoComponent },
     ]),
     BrowserAnimationsModule,
     TuiRoot,
+    NavMenuComponent,
+    HomeComponent,
   ],
   providers: [
     { provide: APP_ID, useValue: 'ng-cli-universal' },
