e24baedd9e77a11d900e554bf4bc32acfd785aad
v.syrov <v.syrov@pix.ru>
2025-03-24 15:46:44 +0300

    Установил Taiga-UI

--- src/Web/ClientApp/src/app/app.module.ts
+++ src/Web/ClientApp/src/app/app.module.ts
@@ -1,10 +1,14 @@
+import { NG_EVENT_PLUGINS } from '@taiga-ui/event-plugins';
+import { TuiRoot } from '@taiga-ui/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-
-import { ModalModule } from 'ngx-bootstrap/modal';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -16,30 +20,32 @@ import { AuthorizeInterceptor } from 'src/api-authorization/authorize.intercepto
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavMenuComponent,
-        HomeComponent,
-        CounterComponent,
-        FetchDataComponent,
-        TodoComponent
-    ],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        RouterModule.forRoot([
-            { path: '', component: HomeComponent, pathMatch: 'full' },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: 'todo', component: TodoComponent }
-        ]),
-        BrowserAnimationsModule,
-        ModalModule.forRoot()],
-    providers: [
-        { provide: APP_ID, useValue: 'ng-cli-universal' },
-        { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true },
-        provideHttpClient(withInterceptorsFromDi())
-    ]
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent,
+    TodoComponent,
+  ],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'counter', component: CounterComponent },
+      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'todo', component: TodoComponent },
+    ]),
+    BrowserAnimationsModule,
+    TuiRoot,
+  ],
+  providers: [
+    { provide: APP_ID, useValue: 'ng-cli-universal' },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true },
+    provideHttpClient(withInterceptorsFromDi()),
+    NG_EVENT_PLUGINS,
+  ],
 })
-export class AppModule { }
+export class AppModule {}
