73409a219dae93060ead253725e3ade0a5d2dfbe
v.syrov <v.syrov@pix.ru>
2025-03-30 16:00:29 +0300

    IT-33 Начата работа над страницей курсов

--- src/Web/ClientApp/src/app/courses-facade.service.ts
+++ src/Web/ClientApp/src/app/courses-facade.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { Course, Student } from './web-api-client';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface CoursesState {
+  coursesList: Course[];
+}
+
+const _state: CoursesState = {
+  coursesList: [
+    new Course({
+      name: 'Course 1',
+      description: 'Course 1',
+      taskBlocks: [],
+      students: [
+        new Student({
+          educationStartYear: 2021,
+          educationalProgram: 'ПМИ',
+          groupNumber: 2,
+        }),
+        new Student({
+          educationStartYear: 2020,
+          educationalProgram: 'БИ',
+          groupNumber: 1,
+        }),
+        new Student({
+          educationStartYear: 2022,
+          educationalProgram: 'ПИ',
+          groupNumber: 3,
+        }),
+        new Student({
+          educationStartYear: 2023,
+          educationalProgram: 'ПИ',
+          groupNumber: 2,
+        }),
+        new Student({
+          educationStartYear: 2020,
+          educationalProgram: 'ПИ',
+          groupNumber: 2,
+        }),
+        new Student({
+          educationStartYear: 2022,
+          educationalProgram: 'ПИ',
+          groupNumber: 2,
+        }),
+      ],
+    }),
+  ],
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesFacadeService {
+  private _store: BehaviorSubject<CoursesState> = new BehaviorSubject(_state);
 
-  constructor() { }
+  coursesList$ = this._store.pipe(map((state) => state.coursesList));
 }
--- src/Web/ClientApp/src/app/teacher/courses-page/courses-page.component.ts
+++ src/Web/ClientApp/src/app/teacher/courses-page/courses-page.component.ts
@@ -1,10 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { TuiTable } from '@taiga-ui/addon-table';
+import { CoursesFacadeService } from '../../courses-facade.service';
+import { AsyncPipe, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-courses-page',
-  imports: [TuiTable],
+  imports: [TuiTable, AsyncPipe],
   templateUrl: './courses-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: ':host { @apply grow flex flex-col *:grow }',
 })
-export class CoursesPageComponent {}
+export class CoursesPageComponent {
+  coursesFacade = inject(CoursesFacadeService);
+  coursesList$ = this.coursesFacade.coursesList$;
+
+  readonly columns = [
+    'course',
+    'groups',
+    'students',
+    'taskBlocks',
+    'description',
+  ];
+}
