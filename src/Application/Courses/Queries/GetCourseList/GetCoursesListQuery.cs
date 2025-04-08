9b4df5e12788a30cd8d4df09d5c15e2cb855ae0b
VladZF <vlad2003587@gmail.com>
2025-04-08 23:32:29 +0300

    Поправлены команды

--- src/Application/Courses/Queries/GetCourseList/GetCoursesListQuery.cs
+++ src/Application/Courses/Queries/GetCourseList/GetCoursesListQuery.cs
@@ -1,13 +1,12 @@
 using Itishnik.Application.Common.Interfaces;
 using Itishnik.Application.Common.Mappings;
 using Itishnik.Application.Common.Models;
-using Itishnik.Domain.Entities;
 
 namespace Itishnik.Application.Courses.Queries.GetCourseList;
 
 public record GetCoursesListQuery(int PageNumber = 1, int PageSize = 10) : IRequest<PaginatedList<CourseListResponse>>;
 
-public class CreateCoursesListQueryHandler(IApplicationDbContext context, IMapper mapper) 
+public class GetCoursesListQueryHandler(IApplicationDbContext context, IMapper mapper) 
     : IRequestHandler<GetCoursesListQuery, PaginatedList<CourseListResponse>>
 {
     private readonly IApplicationDbContext _context = context;
@@ -16,8 +15,7 @@ public class CreateCoursesListQueryHandler(IApplicationDbContext context, IMappe
     public Task<PaginatedList<CourseListResponse>> Handle(GetCoursesListQuery request, CancellationToken cancellationToken)
     {
         return _context.Courses
-            .Include(x => x.Students)
-            .Select(x => _mapper.Map<Course, CourseListResponse>(x))
+            .ProjectTo<CourseListResponse>(_mapper.ConfigurationProvider)
             .PaginatedListAsync(request.PageNumber, request.PageSize);
     }
 }
