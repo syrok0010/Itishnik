16786a1da96f8d3e8792ca22286db53fe324fda6
VladZF <vlad2003587@gmail.com>
2025-04-08 23:31:27 +0300

    Response классы переведены в обычные классы с добавлением внутреннего маппинг класса

--- src/Application/Courses/CourseListResponse.cs
+++ src/Application/Courses/CourseListResponse.cs
@@ -1,3 +1,20 @@
+using Itishnik.Domain.Entities;
+
 namespace Itishnik.Application.Courses;
 
-public record CourseListResponse(Guid Id, string Name, int StudentsCount, string? Description = null);
+public class CourseListResponse
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; } = null!;
+    public int StudentsCount { get; init; }
+    public string? Description { get; init; }
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Course, CourseListResponse>()
+                .ForMember(clr => clr.StudentsCount, options => options.MapFrom(c => c.GradedCourses.Count()));
+        }
+    }
+}
--- src/Application/Courses/TaskBlockResponse.cs
+++ src/Application/Courses/TaskBlockResponse.cs
@@ -1,13 +1,40 @@
+using Itishnik.Domain.Entities;
+using Task = Itishnik.Domain.Entities.Task;
+
 namespace Itishnik.Application.Courses;
 
-public record TaskListDto(Guid Id, string Name);
+public class TaskListDto
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; } = null!;
 
-public record TaskBlockResponse(
-    Guid Id,
-    Guid CourseId,
-    string Name,
-    ICollection<TaskListDto> Tasks,
-    ICollection<int> Weights,
-    DateTime StartTime,
-    DateTime EndTime,
-    TimeSpan TimeAllowed);
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Task, TaskListDto>();
+        }
+    }
+}
+
+public class TaskBlockResponse
+{
+    public Guid Id { get; init; }
+    public Guid CourseId { get; init; }
+    public string Name { get; init; } = null!;
+    public ICollection<TaskListDto> Tasks { get; init; } = null!;
+    public ICollection<int> Weights { get; init; } = null!;
+    public DateTime StartTime { get; init; }
+    public DateTime EndTime { get; init; }
+    public TimeSpan TimeAllowed { get; init; }
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<TaskBlock, TaskBlockResponse>()
+                .ForMember(tbr => tbr.Tasks, options => options.MapFrom(tb => tb.Tasks))
+                .ForMember(tbr => tbr.Weights, options => options.MapFrom(tb => tb.Weights));
+        }
+    }
+}
