71dcba4f0387cde23627be282051e6271c6227aa
v.syrov <v.syrov@pix.ru>
2025-04-16 22:17:37 +0300

    IT-39 Добалена загрузка списка задач и создан метод создания задачи в фасаде

--- src/Web/ClientApp/src/app/tasks-facade.service.ts
+++ src/Web/ClientApp/src/app/tasks-facade.service.ts
@@ -1,45 +1,71 @@
-import { Injectable } from '@angular/core';
-import { Tag, TaskListResponse } from './web-api-client';
-import { BehaviorSubject } from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import {
+  CreateTaskCommand,
+  TaskListResponse,
+  TasksClient,
+} from './web-api-client';
+import { BehaviorSubject, firstValueFrom } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
 export interface TasksState {
   taskList: TaskListResponse[];
 }
 
-const _state: TasksState = {
-  taskList: [
-    new TaskListResponse({
-      id: '',
-      name: 'Some course name',
-      teacherFullName: 'Бычков Илья Сергеевич',
-      teacherEmail: 'ibychkov@hse.ru',
-      isPublic: true,
-      tags: [
-        new Tag({
-          id: '',
-          text: 'Массивы',
-        }),
-        new Tag({
-          id: '',
-          text: 'Стэк',
-        }),
-        new Tag({
-          id: '',
-          text: 'BFS',
-        }),
-      ],
-    }),
-  ],
+let _state: TasksState = {
+  taskList: [],
 };
 
+@UntilDestroy()
 @Injectable({
   providedIn: 'root',
 })
 export class TasksFacadeService {
+  private readonly tasksClient = inject(TasksClient);
+
   private _store: BehaviorSubject<TasksState> = new BehaviorSubject(_state);
 
   taskList$ = this._store.pipe(map((state) => state.taskList));
 
-  createTask(name: string): void {}
+  constructor() {
+    this.tasksClient
+      .getTaskList(1, 50)
+      .pipe(untilDestroyed(this))
+      .subscribe((x) =>
+        this._store.next(
+          (_state = {
+            ..._state,
+            taskList: [..._state.taskList, ...x.items],
+          }),
+        ),
+      );
+  }
+
+  async createTask(
+    name: string,
+    text: string,
+    isPublic: boolean,
+  ): Promise<void> {
+    const response = await firstValueFrom(
+      this.tasksClient.createTaskRequest(
+        new CreateTaskCommand({
+          name,
+          text,
+          isPublic,
+        }),
+      ),
+    );
+
+    this._store.next(
+      (_state = {
+        ..._state,
+        taskList: [
+          ..._state.taskList,
+          new TaskListResponse({
+            ...response,
+          }),
+        ],
+      }),
+    );
+  }
 }
