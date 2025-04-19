6a680e53dacfe55f28aeda82fd7dd97c8acf8b00
v.syrov <v.syrov@pix.ru>
2025-04-19 15:49:08 +0300

    IT-40 Добавлен гард на роль учителя/админа

--- src/Web/ClientApp/src/app/app.routes.ts
+++ src/Web/ClientApp/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { isAdminOrTeacherGuard } from '../api-authorization/auth-guards';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   {
-    path: 'teacher/courses',
+    path: 'courses',
+    canMatch: [isAdminOrTeacherGuard],
     loadComponent: () =>
       import('./teacher/courses-page/courses-page.component'),
   },
