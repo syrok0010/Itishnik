4c5fe7f464e71c42d3070eda8f0d24171e43b080
v.syrov <v.syrov@pix.ru>
2025-04-19 13:02:45 +0300

    IT-39 Изменен внешний вид публичности задачи в таблице

--- src/Web/ClientApp/src/app/teacher/tasks-page/tasks-page.component.ts
+++ src/Web/ClientApp/src/app/teacher/tasks-page/tasks-page.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { TuiTable } from '@taiga-ui/addon-table';
 import { TasksFacadeService } from '../../tasks-facade.service';
 import { AsyncPipe } from '@angular/common';
-import { TuiAvatar, TuiCheckbox, TuiChip } from '@taiga-ui/kit';
+import { TuiAvatar, TuiBadge, TuiChip, TuiStatus } from '@taiga-ui/kit';
 import { TuiAutoColorPipe, TuiInitialsPipe, TuiTitle } from '@taiga-ui/core';
 import { TuiCell } from '@taiga-ui/layout';
 import { FormsModule } from '@angular/forms';
@@ -19,9 +19,10 @@ import { RouterLink } from '@angular/router';
     TuiInitialsPipe,
     TuiTitle,
     TuiCell,
-    TuiCheckbox,
     FormsModule,
     RouterLink,
+    TuiBadge,
+    TuiStatus,
   ],
   templateUrl: './tasks-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
