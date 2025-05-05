679d6ccb2a11eb53513d86fe80f25764c285d404
VladZF <vlad2003587@gmail.com>
2025-05-05 21:50:07 +0300

    IT-18 Удалена политика CourseOwner и user из команды

--- src/Application/Courses/Queries/GetStudentsOnCourse/GetStudentsOnCourseQuery.cs
+++ src/Application/Courses/Queries/GetStudentsOnCourse/GetStudentsOnCourseQuery.cs
@@ -9,15 +9,15 @@ namespace Itishnik.Application.Courses.Queries.GetStudentsOnCourse;
 [ResourceMetadata(nameof(Id), typeof(Course))]
 public record GetStudentsOnCourseQuery(Guid Id) : IRequest<CourseStudentListResponse>;
 
-public class GetStudentsOnCourseQueryHandler(IApplicationDbContext context, IMapper mapper, IUser user) : IRequestHandler<GetStudentsOnCourseQuery, CourseStudentListResponse>
+public class GetStudentsOnCourseQueryHandler(IApplicationDbContext context, IMapper mapper) : IRequestHandler<GetStudentsOnCourseQuery, CourseStudentListResponse>
 {
     private readonly IApplicationDbContext _context = context;
     private readonly IMapper _mapper = mapper;
-    private readonly IUser _user = user;
     
     public async Task<CourseStudentListResponse> Handle(GetStudentsOnCourseQuery request, CancellationToken cancellationToken)
     {
         var students = await _context.Students
+            .AsNoTracking()
             .Where(s => s.GradedCourses.Any(gc => gc.CourseId == request.Id))
             .ProjectTo<StudentDto>(_mapper.ConfigurationProvider)
             .ToListAsync(cancellationToken);
