b6d7133e7f2b86fbf0619da8c52a59b926800946
VladZF <vlad2003587@gmail.com>
2025-05-07 18:08:42 +0300

    IT-26 Оптимизирована валидация

--- src/Application/Courses/Commands/ChangeTaskBlockDescription/ChangeTaskBlockDescriptionCommandValidator.cs
+++ src/Application/Courses/Commands/ChangeTaskBlockDescription/ChangeTaskBlockDescriptionCommandValidator.cs
@@ -4,25 +4,13 @@ namespace Itishnik.Application.Courses.Commands.ChangeTaskBlockDescription;
 
 public class ChangeTaskBlockDescriptionCommandValidator : AbstractValidator<ChangeTaskBlockDescriptionCommand>
 {
-    private readonly IApplicationDbContext _context;
-    
     public ChangeTaskBlockDescriptionCommandValidator(IApplicationDbContext context)
     {
-        _context = context;
         RuleFor(x => x.CourseId)
-            .MustAsync((id, token) => _context.Courses.AnyAsync(c => c.Id == id, token))
+            .MustAsync((id, token) => context.Courses.AnyAsync(c => c.Id == id, token))
             .WithMessage("Курса не существует");
         RuleFor(x => x.TaskBlockId)
-            .MustAsync((id, token) => _context.TaskBlocks.AnyAsync(tb => tb.Id == id, token))
-            .WithMessage("Блока задач не существует");
-        RuleFor(x => x)
-            .MustAsync((command, token) => CheckRelation(command.CourseId, command.TaskBlockId, token))
-            .WithMessage("Блок задач не принадлежит курсу");
-    }
-    
-    private async Task<bool> CheckRelation(Guid courseId, Guid blockId, CancellationToken cancellationToken)
-    {
-        var block = await _context.TaskBlocks.FirstAsync(x => x.Id == blockId, cancellationToken);
-        return block.CourseId == courseId;
+            .MustAsync((cmd, id, token) => context.TaskBlocks.AnyAsync(tb => tb.Id == id && tb.CourseId == cmd.CourseId, token))
+            .WithMessage("Блока задач не существует или не принадлежит заданному курсу");
     }
 }
