f2f18d7023e7e2b63247a706a0c83c59f226398c
v.syrov <v.syrov@pix.ru>
2025-05-08 13:01:02 +0300

    IT-44 Исправлены конфликты после актуализации ветки

--- src/Web/Infrastructure/IEndpointRouteBuilderExtensions.cs
+++ src/Web/Infrastructure/IEndpointRouteBuilderExtensions.cs
@@ -25,17 +25,6 @@ public static class IEndpointRouteBuilderExtensions
 
         return builder;
     }
-    
-    public static IEndpointRouteBuilder MapPatch(this IEndpointRouteBuilder builder, Delegate handler,
-        [StringSyntax("Route")] string pattern = "")
-    {
-        Guard.Against.AnonymousMethod(handler);
-
-        builder.MapPatch(pattern, handler)
-            .WithName(handler.Method.Name);
-
-        return builder;
-    }
 
     public static IEndpointRouteBuilder MapPut(this IEndpointRouteBuilder builder, Delegate handler,
         [StringSyntax("Route")] string pattern)
