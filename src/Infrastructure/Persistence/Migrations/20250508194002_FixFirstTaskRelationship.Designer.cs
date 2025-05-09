970bd37a842145e00143493ce7964e4a68d90b81
v.syrov <v.syrov@pix.ru>
2025-05-09 14:39:43 +0300

    IT-44 Исправлено отношениее задачи-первой задачи

--- src/Infrastructure/Persistence/Migrations/20250508194002_FixFirstTaskRelationship.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20250508194002_FixFirstTaskRelationship.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Itishnik.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Itishnik.Infrastructure.Data.Migrations
+namespace Itishnik.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250508194002_FixFirstTaskRelationship")]
+    partial class FixFirstTaskRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
