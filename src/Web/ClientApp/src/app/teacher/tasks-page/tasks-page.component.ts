48aedbdf1fd6bb559bfb254513e62892ef1ceec2
v.syrov <v.syrov@pix.ru>
2025-05-18 16:50:16 +0300

    IT-52 Таблица с фильтрами вынесена в отдельный компонент

--- src/Web/ClientApp/src/app/teacher/tasks-page/tasks-page.component.ts
+++ src/Web/ClientApp/src/app/teacher/tasks-page/tasks-page.component.ts
@@ -1,75 +1,17 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  signal,
-} from '@angular/core';
-import { TuiSortChange, TuiTable } from '@taiga-ui/addon-table';
-import { FilterState, TasksFacadeService } from '../../tasks-facade.service';
-import { AsyncPipe } from '@angular/common';
-import { TuiAvatar, TuiBadge, TuiChip, TuiStatus } from '@taiga-ui/kit';
-import { TuiAutoColorPipe, TuiInitialsPipe, TuiTitle } from '@taiga-ui/core';
-import { TuiCell } from '@taiga-ui/layout';
-import {
-  FormControl,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { RouterLink } from '@angular/router';
-import TagMultiselectInputComponent from '../../components/tag-multiselect-input.component';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TuiInputModule } from '@taiga-ui/legacy';
-import UserMultiselectInputComponent from '../../components/user-multiselect-input.component';
-import { Role } from '../../users-facade.service';
-import { TaskListResponse } from '../../web-api-client';
+import TaskTableComponent from '../../components/task-table/task-table.component';
 
 @Component({
   selector: 'app-tasks-page',
   imports: [
-    TuiTable,
-    AsyncPipe,
-    TuiChip,
-    TuiAutoColorPipe,
-    TuiAvatar,
-    TuiInitialsPipe,
-    TuiTitle,
-    TuiCell,
     FormsModule,
-    RouterLink,
-    TuiBadge,
-    TuiStatus,
-    TagMultiselectInputComponent,
     ReactiveFormsModule,
     TuiInputModule,
-    UserMultiselectInputComponent,
+    TaskTableComponent,
   ],
-  templateUrl: './tasks-page.component.html',
+  template: `<app-task-table />`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class TasksPageComponent {
-  taskFacade = inject(TasksFacadeService);
-
-  currentSortBy = signal<string | null>(null);
-  roles = ['Teacher'] as Role[];
-  filterForm = new FormGroup({
-    name: new FormControl(''),
-    authorIds: new FormControl<string[]>([]),
-    tagIds: new FormControl<string[]>([]),
-  });
-
-  columns = ['isPublic', 'name', 'author', 'tags'] as readonly string[];
-
-  constructor() {
-    this.filterForm.valueChanges
-      .pipe(takeUntilDestroyed())
-      .subscribe((filters) =>
-        this.taskFacade.setFilters(filters as FilterState),
-      );
-  }
-
-  sortChanged(e: TuiSortChange<Partial<Record<keyof TaskListResponse, any>>>) {
-    this.taskFacade.setSorting(e.sortKey, e.sortDirection === 1);
-    this.currentSortBy.set(e.sortKey);
-  }
-}
+export default class TasksPageComponent {}
