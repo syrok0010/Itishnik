17740fc00513ab6337019cd41a4655663d046990
VladZF <vlad2003587@gmail.com>
2025-05-19 23:21:34 +0300

    IT-28 Удалены лишние условия

--- src/Application/Courses/Commands/PublishTaskBlock/PublishTaskBlockCommandValidator.cs
+++ src/Application/Courses/Commands/PublishTaskBlock/PublishTaskBlockCommandValidator.cs
@@ -10,10 +10,10 @@ public class PublishTaskBlockCommandValidator : AbstractValidator<PublishTaskBlo
             .MustAsync((id, token) => context.Courses.AnyAsync(c => c.Id == id, token))
             .WithMessage("Курса не существует");
         RuleFor(x => x.TaskBlockId)
+            .Cascade(CascadeMode.Stop)
             .MustAsync((cmd, id, token) =>
                 context.TaskBlocks.AnyAsync(tb => tb.Id == id && tb.CourseId == cmd.CourseId && !tb.IsPublic, token))
-            .WithMessage("Блок задач не существует, не принадлежит заданному курсу или опубликован");
-        RuleFor(x => x.TaskBlockId)
+            .WithMessage("Блок задач не существует, не принадлежит заданному курсу или уже опубликован")
             .MustAsync((cmd, id, token) => CheckTasksAndWeights(context, cmd.CourseId, id, token))
             .WithMessage("Блок задач пуст или сумма весов не равна 10");
     }
@@ -25,6 +25,6 @@ public class PublishTaskBlockCommandValidator : AbstractValidator<PublishTaskBlo
             .Where(tb => tb.Id == taskBlockId && tb.CourseId == courseId)
             .Include(tb => tb.Tasks)
             .FirstAsync(cancellationToken);
-        return taskBlock.Tasks.Any(t => t.IsPublic) && taskBlock.Weights.Sum() == 10;
+        return taskBlock.Tasks.Any() && taskBlock.Weights.Sum() == 10;
     }
 }
