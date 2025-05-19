a45f2e38f7e1fede0dc5293f0c2027d288bfcfd9
v.syrov <v.syrov@pix.ru>
2025-05-19 18:28:02 +0300

    IT-53 Закончена локализация

--- src/Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ src/Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,40 +1,23 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
 using Itishnik.Domain.Entities;
-using Itishnik.Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Itishnik.Web.Areas.Identity.Pages.Account
+namespace Itishnik.Web.Areas.Identity.Pages.Account;
+
+public class LogoutModel(SignInManager<ApplicationUser> signInManager, ILogger<LogoutModel> logger) : PageModel
 {
-    public class LogoutModel : PageModel
+    private readonly SignInManager<ApplicationUser> _signInManager = signInManager;
+    private readonly ILogger<LogoutModel> _logger = logger;
+
+    public async Task<IActionResult> OnPost(string returnUrl = null)
     {
-        private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly ILogger<LogoutModel> _logger;
-
-        public LogoutModel(SignInManager<ApplicationUser> signInManager, ILogger<LogoutModel> logger)
-        {
-            _signInManager = signInManager;
-            _logger = logger;
-        }
-
-        public async Task<IActionResult> OnPost(string returnUrl = null)
-        {
-            await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
-            {
-                return LocalRedirect(returnUrl);
-            }
-            else
-            {
-                // This needs to be a redirect so that the browser performs a new
-                // request and the identity for the user gets updated.
-                return RedirectToPage();
-            }
-        }
+        await _signInManager.SignOutAsync();
+        _logger.LogInformation("User logged out.");
+        return returnUrl != null 
+            ? LocalRedirect(returnUrl) 
+            : RedirectToPage();
     }
 }
