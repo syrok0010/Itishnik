5e8aaa3a950b203cee46093959c90481b2eed696
v.syrov <v.syrov@pix.ru>
2025-05-22 21:33:29 +0300

    IT-50 Создана страница и добавлена ссылка на кнопку

---
+++ src/Web/ClientApp/src/app/teacher/create-task-page/create-task-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-task-page',
+  imports: [],
+  templateUrl: './create-task-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class CreateTaskPageComponent {}
--- src/Web/ClientApp/src/app/teacher/tasks-page.component.ts
+++ src/Web/ClientApp/src/app/teacher/tasks-page.component.ts
@@ -3,6 +3,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TuiInputModule } from '@taiga-ui/legacy';
 import TaskTableComponent from '../components/task-table/task-table.component';
 import { TuiButton, TuiIcon } from '@taiga-ui/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks-page',
@@ -13,6 +14,7 @@ import { TuiButton, TuiIcon } from '@taiga-ui/core';
     TaskTableComponent,
     TuiButton,
     TuiIcon,
+    RouterLink,
   ],
   styles: [':host { @apply relative flex grow flex-col }'],
   template: ` <app-task-table />
@@ -21,6 +23,7 @@ import { TuiButton, TuiIcon } from '@taiga-ui/core';
       tuiButton
       type="button"
       class="!absolute bottom-4 right-4 shadow-sm"
+      [routerLink]="['create']"
     >
       <tui-icon icon="@tui.plus" />
     </button>`,
