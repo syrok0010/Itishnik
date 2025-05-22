36faa3b0145ea2a83c90dd09aab761ca08ac40be
v.syrov <v.syrov@pix.ru>
2025-05-22 18:54:02 +0300

    IT-12 Миграция в соответствии с новой конфигурацией

---
+++ src/Infrastructure/Data/Migrations/20250522155320_TaskTaskBlocksRelation.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Itishnik.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TaskTaskBlocksRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskTaskBlock_TaskBlocks_TaskBlockId",
+                table: "TaskTaskBlock");
+
+            migrationBuilder.RenameColumn(
+                name: "TaskBlockId",
+                table: "TaskTaskBlock",
+                newName: "TaskBlocksId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskTaskBlock_TaskBlocks_TaskBlocksId",
+                table: "TaskTaskBlock",
+                column: "TaskBlocksId",
+                principalTable: "TaskBlocks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskTaskBlock_TaskBlocks_TaskBlocksId",
+                table: "TaskTaskBlock");
+
+            migrationBuilder.RenameColumn(
+                name: "TaskBlocksId",
+                table: "TaskTaskBlock",
+                newName: "TaskBlockId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskTaskBlock_TaskBlocks_TaskBlockId",
+                table: "TaskTaskBlock",
+                column: "TaskBlockId",
+                principalTable: "TaskBlocks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
