d04ca9c6061562d8adfa13561948bd30866786a5
v.syrov <v.syrov@pix.ru>
2025-05-22 21:10:08 +0300

    IT-50 Добавлена кнопка создания задания

--- src/Web/ClientApp/src/app/teacher/tasks-page.component.ts
+++ src/Web/ClientApp/src/app/teacher/tasks-page.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TuiInputModule } from '@taiga-ui/legacy';
 import TaskTableComponent from '../components/task-table/task-table.component';
+import { TuiButton, TuiIcon } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-tasks-page',
@@ -10,8 +11,19 @@ import TaskTableComponent from '../components/task-table/task-table.component';
     ReactiveFormsModule,
     TuiInputModule,
     TaskTableComponent,
+    TuiButton,
+    TuiIcon,
   ],
-  template: `<app-task-table />`,
+  styles: [':host { @apply relative flex grow flex-col }'],
+  template: ` <app-task-table />
+    <button
+      appearance="primary"
+      tuiButton
+      type="button"
+      class="!absolute bottom-4 right-4 shadow-sm"
+    >
+      <tui-icon icon="@tui.plus" />
+    </button>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class TasksPageComponent {}
